8a3e9e51fe4417a9c49ab6b7d5cbd2b5965a3886
Eugene Kozlov <kozlovea8@gmail.com>
2017-07-11 19:06:38 +0300

    [fix]

--- lib/math_methods.rb
+++ lib/math_methods.rb
@@ -2,6 +2,8 @@ require "math_methods/version"
 require "math_methods/bords_method"
 require "math_methods/condorset"
 require "math_methods/concordation"
+require "math_methods/gulyanitzky"
+require "math_methods/spectral"
 
 module MathMethods
   # Your code goes here...
