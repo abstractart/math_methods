3a6fec3312101e69e487527d89e67c29c7d1677d
Eugene Kozlov <kozlovea8@gmail.com>
2018-06-03 10:45:31 +0300

    add cardinal posteriori specs and fix posteriori algo for soft ranges

--- lib/math_methods/new_posteriori_competitions.rb
+++ lib/math_methods/new_posteriori_competitions.rb
@@ -5,8 +5,8 @@ class MathMethods::NewPosterioriCompetition
 
   def initialize(task, result_range, task_type)
     @task = task
-    @result_range = result_range.invert
-    set_rejection(task.alternatives, task_type) 
+    @result_range = convert_range(result_range)
+    set_rejection(task.alternatives, task_type)
   end
 
   def get_new_coefficients
@@ -15,7 +15,7 @@ class MathMethods::NewPosterioriCompetition
       rejections[e] = rejection.get(task.ratings.for_expert(e), result_range)
     end
     d_t_max = rejections.values.max
-    
+
     coefficients = {}
     task.experts.each do |e|
       coefficients[e] = new_competition(
@@ -38,7 +38,7 @@ class MathMethods::NewPosterioriCompetition
       @rejection = MathMethods::NewCardinalRejection.new(alternatives)
     end
   end
-  
+
   def b(c1 = 0.9)
     2 * Math.log(c1 / (1 - c1))
   end
@@ -47,7 +47,7 @@ class MathMethods::NewPosterioriCompetition
     50
   end
 
-    
+
   def new_competition(potential, b)
     1.0 / (1.0 + Math.exp(- b * (potential - 0.5)))
   end
@@ -59,6 +59,18 @@ class MathMethods::NewPosterioriCompetition
 
   def new_potential(potential, d_t, d_t_max)
     potential + 0.5 / t - 1.0 / t * d_t / d_t_max
+  end
+
+  def convert_range(result_range)
+    result = {}
+
+    result_range.each do |range, alternatives|
+      Array(alternatives).each do |alternative|
+        result[alternative] = range
+      end
+    end
+
+    result
   end
 end
 
--- spec/new_posteriori_spec.rb
+++ spec/new_posteriori_spec.rb
@@ -1,18 +1,18 @@
 require "spec_helper"
 
 RSpec.describe MathMethods::NewPosterioriCompetition do
-  context "simple task" do
+  context "ordinal task" do
     it "works well" do
       experts = [:e1, :e2, :e3, :e4]
       alternatives = [:a1, :a2, :a3, :a4, :a5]
-      competitions = 
+      competitions =
       [
         { expert: :e1, cf: 0.4 },
         { expert: :e2, cf: 0.3 },
         { expert: :e3, cf: 0.2 },
         { expert: :e4, cf: 0.1 }
       ]
-      ratings = 
+      ratings =
       [
         {
           expert: :e1,
@@ -53,14 +53,240 @@ RSpec.describe MathMethods::NewPosterioriCompetition do
             { alternative: :a4, rating: 5 },
             { alternative: :a5, rating: 4 }
           ]
-        }           
+        }
       ]
       result_range = { 4 => :a1, 3 => :a2, 1 => :a3, 2 => :a4, 5 => :a5 }
-      
+
       task = MathMethods::TaskModel.new(alternatives, experts, competitions, ratings)
       c = MathMethods::NewPosterioriCompetition.new(task, result_range, :ordinal)
-      
+
       expect(c.get_new_coefficients).to eq({ e1: 0.3895, e2: 0.2909, e3: 0.2035, e4: 0.098 })
+    end
+
+    it "works well with soft ranges" do
+      e = [:e1, :e2, :e3, :e4]
+
+      c =
+      [
+        {
+          expert: :e1,
+          cf: 0.4
+        },
+        {
+          expert: :e2,
+          cf: 0.3
+        },
+        {
+          expert: :e3,
+          cf: 0.2
+        },
+        {
+          expert: :e4,
+          cf: 0.1
+        }
+      ]
+
+      a = [:a1, :a2, :a3, :a4, :a5]
+
+      r =
+      [
+        {
+          expert: :e1,
+          values: [
+            {
+              alternative: :a1,
+              rating: 1
+            },
+            {
+              alternative: :a2,
+              rating: 2
+            },
+            {
+              alternative: :a3,
+              rating: 3
+            },
+            {
+              alternative: :a4,
+              rating: 4
+            },
+            {
+              alternative: :a5,
+              rating: 5
+            }
+          ]
+        },
+        {
+          expert: :e2,
+          values: [
+            {
+              alternative: :a1,
+              rating: 5
+            },
+            {
+              alternative: :a2,
+              rating: 4
+            },
+            {
+              alternative: :a3,
+              rating: 3
+            },
+            {
+              alternative: :a4,
+              rating: 2
+            },
+            {
+              alternative: :a5,
+              rating: 1
+            }
+          ]
+        },
+        {
+          expert: :e3,
+          values: [
+            {
+              alternative: :a1,
+              rating: 4
+            },
+            {
+              alternative: :a2,
+              rating: 3
+            },
+            {
+              alternative: :a3,
+              rating: 2
+            },
+            {
+              alternative: :a4,
+              rating: 1
+            },
+            {
+              alternative: :a5,
+              rating: 5
+            }
+          ]
+        },
+        {
+          expert: :e4,
+          values: [
+            {
+              alternative: :a1,
+              rating: 3
+            },
+            {
+              alternative: :a2,
+              rating: 2
+            },
+            {
+              alternative: :a3,
+              rating: 1
+            },
+            {
+              alternative: :a4,
+              rating: 5
+            },
+            {
+              alternative: :a5,
+              rating: 4
+            }
+          ]
+        }
+      ]
+      result_range = {1=>[:a3], 2=>[:a2], 3=>[:a1, :a4], 4=>[:a5]}
+
+      task = MathMethods::TaskModel.new(a, e, c, r)
+      c = MathMethods::NewPosterioriCompetition.new(task, result_range, :ordinal)
+
+      expect(c.get_new_coefficients).to eq({ e1: 0.3979, e2: 0.2909, e3: 0.1986, e4: 0.1024 })
+    end
+  end
+
+  context 'cardinal task' do
+    let (:task) {
+      MathMethods::CardinalTask.new(
+        [:a, :b],
+        [:e1, :e2, :e3, :e4, :e5, :e6, :e7],
+        [
+          { expert: :e1, cf: 0.25 },
+          { expert: :e2, cf: 0.23 },
+          { expert: :e3, cf: 0.17 },
+          { expert: :e4, cf: 0.15 },
+          { expert: :e5, cf: 0.1 },
+          { expert: :e6, cf: 0.05 },
+          { expert: :e7, cf: 0.05 },
+        ],
+        [
+          {
+            expert: :e1,
+            values: [
+              { alternative: :a, rating: 8 },
+              { alternative: :b, rating: 3 }
+            ]
+          },
+          {
+            expert: :e2,
+            values: [
+              { alternative: :a, rating: 8 },
+              { alternative: :b, rating: 5 }
+            ]
+          },
+          {
+            expert: :e3,
+            values: [
+              { alternative: :a, rating: 8 },
+              { alternative: :b, rating: 7 }
+            ]
+          },
+          {
+            expert: :e4,
+            values: [
+              { alternative: :a, rating: 8 },
+              { alternative: :b, rating: 7 }
+            ]
+          },
+          {
+            expert: :e5,
+            values: [
+              { alternative: :a, rating: 8 },
+              { alternative: :b, rating: 3 }
+            ]
+          },
+          {
+            expert: :e6,
+            values: [
+              { alternative: :a, rating: 7 },
+              { alternative: :b, rating: 5 }
+            ]
+          },
+          {
+            expert: :e7,
+            values: [
+              { alternative: :a, rating: 9 },
+              { alternative: :b, rating: 5 }
+            ]
+          }
+        ],
+        [],
+        10
+      )
+    }
+
+    it 'simple task' do
+      result_range = {8 => :a, 4.94 => :b}
+
+      c = MathMethods::NewPosterioriCompetition.new(task, result_range, :cardinal)
+
+      expect(c.get_new_coefficients).to eq(
+        {
+          :e1 => 0.2428,
+          :e2 => 0.2374,
+          :e3 => 0.1639,
+          :e4 => 0.1445,
+          :e5 => 0.0966,
+          :e6 => 0.0501,
+          :e7 => 0.0501
+        }
+      )
+
     end
   end
 end
